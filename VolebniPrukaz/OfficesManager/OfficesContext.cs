fb0fc4dee604397beeb643069a41bfb8ed4ec518
David Roško <rosko.dav@gmail.com>
2017-09-26 15:11:15 +0200

    Refactor

--- VolebniPrukaz/OfficesManager/OfficesContext.cs
+++ VolebniPrukaz/OfficesManager/OfficesContext.cs
@@ -1,19 +1,14 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using VolebniPrukaz;
 
 namespace VolebniPrukaz.OfficesManager
 {
     public class OfficesContext
     {
-        private string _file { get; set; }
-
         /// <summary>
-        /// 
         /// </summary>
         /// <param name="file">Json file</param>
         public OfficesContext(string filePath)
@@ -21,14 +16,15 @@ namespace VolebniPrukaz.OfficesManager
             _file = filePath;
         }
 
+        private string _file { get; }
+
         public List<Office> GetOffices(int zip)
         {
-            using (StreamReader file = File.OpenText(_file))
-            using (JsonTextReader reader = new JsonTextReader(file))
+            using (var file = File.OpenText(_file))
+            using (var reader = new JsonTextReader(file))
             {
-                var obj = JToken.ReadFrom(reader);
-                var deserialize = obj.ToObject<List<Office>>();
-                return deserialize.Where(a=> a.zip == zip.ToString()).ToList();
+                var collection = JToken.ReadFrom(reader).ToObject<List<Office>>();
+                return collection.Where(a => a.zip == zip.ToString()).ToList();
             }
         }
     }
