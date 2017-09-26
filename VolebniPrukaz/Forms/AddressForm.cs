1659c5e2226d1ef3713252e7f8b9b82a8e406af3
David Roško <rosko.dav@gmail.com>
2017-09-26 17:13:45 +0200

    Add validation

--- VolebniPrukaz/Forms/AddressForm.cs
+++ VolebniPrukaz/Forms/AddressForm.cs
@@ -21,33 +21,53 @@ namespace VolebniPrukaz.Forms
                             IsValid = true,
                             Value = response
                         };
-
                         return result;
-                        await Task.CompletedTask;
                     })
                 .Field(nameof(AddressDM.Zip),
                     prompt: "Zadejte prosím PSČ.",
-                    validate: async (state, response) => {
-                        ValidateResult result = new ValidateResult
+                    validate: async (state, response) =>
+                    {
+                        string zipStr = (string) response;
+                        if (int.TryParse((string) response, out int zip) &&
+                            (zipStr.Trim().Replace(" ", "").Length == 5))
                         {
-                            IsValid = true,
-                            Value = response
-                        };
-
-                        return result;
-                        await Task.CompletedTask;
+                            return new ValidateResult
+                            {
+                                IsValid = true,
+                                Value = response
+                            };
+                        }
+                        else
+                        {
+                            return new ValidateResult
+                            {
+                                IsValid = false,
+                                Value = response,
+                                Feedback = "Tak tomu nerozumím, potřebuji smerovací číslo, například ve formátu 130 00"
+                            };
+                        }
                     })
                 .Field(nameof(AddressDM.Street),
                     prompt: "Zadejte prosím ulici (bez čísla domu).",
                     validate: async (state, response) => {
-                        ValidateResult result = new ValidateResult
+                        string adress = (string)response;
+                        if (adress.Length >= 2)
                         {
-                            IsValid = true,
-                            Value = response
-                        };
-
-                        return result;
-                        await Task.CompletedTask;
+                            return new ValidateResult
+                            {
+                                IsValid = true,
+                                Value = response
+                            };
+                        }
+                        else
+                        {
+                            return new ValidateResult
+                            {
+                                IsValid = false,
+                                Value = response,
+                                Feedback = "Takovou ulici neznam. Asi mě zkoušíte že?"
+                            };
+                        }
                     })
                 .Field(nameof(AddressDM.HouseNumber),
                     prompt: "Zadejte prosím číslo domu.",
@@ -57,9 +77,7 @@ namespace VolebniPrukaz.Forms
                             IsValid = true,
                             Value = response
                         };
-
                         return result;
-                        await Task.CompletedTask;
                     })
                 .Build();
         }
